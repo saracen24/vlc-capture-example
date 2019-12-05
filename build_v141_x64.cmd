eda046184c740928758171f248706311da36a758
saracen24 <oppressor24@gmail.com>
2019-12-05 18:24:19 +0300

    Fix MSVC v141 build script.

--- build_v141_x64.cmd
+++ build_v141_x64.cmd
@@ -12,8 +12,6 @@ pushd "build_v141_x64"
 
 :: Setup developer command prompt for Visual Studio
 call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/Tools/VsDevCmd.bat" -arch=x64
-:: Setup the environement for VS 2017 x64
-call "%VS140COMNTOOLS%../../VC/vcvarsall.bat" x64
 
 :: Configure and build
 cmake .. -G "Visual Studio 15 2017 Win64" -T host=x64
